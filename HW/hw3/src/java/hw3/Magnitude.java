f7a252b2b25e8bcc28181e576144be3feb7be3f4
Marco Munizaga <marco.munizaga@ufl.edu>
2013-09-17 17:23:20 -0400

    update

--- HW/hw3/src/java/hw3/Magnitude.java
+++ HW/hw3/src/java/hw3/Magnitude.java
@@ -25,3 +25,39 @@ public abstract class Magnitude {
     }
 
 }
+
+
+---------------------
+
+package hw3;
+
+public class Currency extends Magnitude{
+    protected double money;
+    
+    public Currency(double money){
+        this.money = money;
+    }
+    
+    public boolean lessThan(Magnitude m){
+        double money = ( (Currency) m).money;
+        return this.money < money;
+    }
+}
+
+----------------------
+
+package hw3;
+
+public class CartPoint extends Magnitude{
+    protected double magnitude;
+    
+    public CartPoint(double x, double y){
+        this.magnitude = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+    }
+    
+    public boolean lessThan(Magnitude m){
+        double magnitude = ( (CartPoint) m).magnitude;
+        return this.magnitude < magnitude;
+    }
+}
+
